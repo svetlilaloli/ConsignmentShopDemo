25a776587edaba92419c4f451b9e091ad917b5cc
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-01-07 13:08:09 +0000

    Add files via upload

---
+++ ConsignmentShopLibrary/Store.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ConsignmentShopLibrary
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public List<Vendor> Vendors { get; set; }
+        public List<Item> Items { get; set; }
+        public Store()
+        {
+            Vendors = new List<Vendor>();
+            Items = new List<Item>();
+        }
+    }
+}
---
+++ ConsignmentShopLibrary/Vendor.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsignmentShopLibrary
+{
+    public class Vendor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Commission { get; set; }
+        public decimal PaymentDue { get; set; }
+        public Vendor()
+        {
+            Commission = .5;
+            PaymentDue = .0M;
+        }
+        public string Display
+        {
+            get
+            {
+                return string.Format("{0} {1} - ${2:f2}", FirstName, LastName, PaymentDue);
+            }
+        }
+    }
+}
