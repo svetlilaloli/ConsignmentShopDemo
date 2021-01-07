0d3b3f4ab019e87bc146ec8b88809b961bcdb1e1
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-01-07 13:07:21 +0000

    Create Item.cs

---
+++ ConsignmentShopLibrary/Item.cs
@@ -0,0 +1,19 @@
+namespace ConsignmentShopLibrary
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool Sold { get; set; }
+        public bool PaymentDistributed { get; set; }
+        public Vendor Owner { get; set; }
+        public string Display
+        {
+            get
+            {
+                return string.Format("{0} - ${1:f2}", Title, Price);
+            }
+        }
+    }
+}
